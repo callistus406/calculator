e2206f6e44e43219b49552b2de748cf63ebbfd4e
callistus406 <callistus406@gmail.com>
2020-10-08 16:49:56 +0100

    changed few things

--- js/calculator.js
+++ js/calculator.js
@@ -1,51 +1,97 @@
 let numbers = document.querySelector(".numbers");
 let display = document.querySelector("#display");
-//let addition = document.querySelector("#addition")
+let capture = document.querySelector(".capture");
+let storeVal;
+// const activate = function () {
+//   let initialSize = 20 - display.value.length;
+//   initialSize = initialSize <= 15 ? 15 : initialSize;
+//   display.style.fontSize = initialSize + "px";
+//   console.log(display.value.length);
 
-(function(){
-  numbers.addEventListener("click",e=>{
-    if (e.target.textContent == "1"  )return display.value +=e.target.textContent; 
-    if (e.target.textContent == "2"  )return display.value +=e.target.textContent; 
-    if (e.target.textContent == "3"  )return  display.value +=e.target.textContent ; 
-    if (e.target.textContent == "4"  )return display.value +=e.target.textContent; 
-    if (e.target.textContent == "5"  )return display.value +=e.target.textContent; 
-    if (e.target.textContent == "6"  )return display.value +=e.target.textContent; 
-    if (e.target.textContent == "7"  )return display.value +=e.target.textContent; 
-    if (e.target.textContent == "8"  )return display.value +=e.target.textContent; 
-    if (e.target.textContent == "9"  )return display.value +=e.target.textContent; 
-    if (e.target.textContent == "0"  )return display.value +=e.target.textContent; 
-    if (e.target.textContent == "CE"  )return clearScreen(); 
-    if (e.target.classList.contains("backspace") )return deleteNumbers(); 
-     if (e.target.textContent == "+" )return customOperator("+"); 
-     if (e.target.textContent == "-" )return customOperator("-"); 
-     if (e.target.classList.contains("multiplication") )return customOperator("*");  
-     if (e.target.classList.contains("division") )return customOperator("/"); 
-     if(e.target.textContent == "=")return equal(e);
+// }
+// display.addEventListener("onkeyup", activate);
+
+
+(function () {
+  numbers.addEventListener("click", e => {
+
+    if (e.target.textContent == "1") return display.value += e.target.textContent;
+    if (e.target.textContent == "2") return display.value += e.target.textContent;
+    if (e.target.textContent == "3") return display.value += e.target.textContent;
+    if (e.target.textContent == "4") return display.value += e.target.textContent;
+    if (e.target.textContent == "5") return display.value += e.target.textContent;
+    if (e.target.textContent == "6") return display.value += e.target.textContent;
+    if (e.target.textContent == "7") return display.value += e.target.textContent;
+    if (e.target.textContent == "8") return display.value += e.target.textContent;
+    if (e.target.textContent == "9") return display.value += e.target.textContent;
+    if (e.target.textContent == "0") return display.value += e.target.textContent;
+    if (e.target.textContent == "CE") return clearScreen();
+    if (e.target.classList.contains("backspace")) return deleteNumbers();
+    if (e.target.textContent == "+") return customOperator("+");
+    if (e.target.textContent == "-") return customOperator("-");
+    if (e.target.classList.contains("multiplication")) return customOperator("*");
+    if (e.target.classList.contains("division")) return customOperator("/");
+    // if (e.target.textContent == "(") return signOperator("(");
+    // if (e.target.textContent == ")") return signOperator(")");
+    if (e.target.classList.contains("pi")) return display.value += 3.14159265358;
+    if (e.target.textContent == "Mod") return customOperator("Mod");
+    // if (e.target.textContent == "n!") return customOperator("!");
+
+    if (e.target.textContent == "=") return equal(e);
   });
 })();
 
-const clearScreen = function(){
-  
-  display.value="";
+const clearScreen = function () {
+  display.value = "";
 }
 
-const deleteNumbers = function(){
+const deleteNumbers = function () {
   let arr = display.value;
   display.value = arr.slice(1);
 }
 
 let initVal;
 let mathOperator;
-const customOperator = function(operator){
+let storeSign;
+const signOperator = function (operator) {
+
   initVal = +display.value;
+  capture.innerHTML = initVal + " " + operator;
+
+  return mathOperator = operator;
+}
+const customOperator = function (operator) {
+  initVal = +display.value;
+  capture.innerHTML = initVal + " " + operator;
   display.value = "";
-   return mathOperator = operator;
+  return mathOperator = operator;
 }
 
-const equal = function(){
-  let getValue = +display.value;
-  if( mathOperator == "+" ) return display.value = initVal + getValue;
-  if( mathOperator == "-" ) return display.value = initVal - getValue;
-  if( mathOperator == "*" ) return display.value =  initVal * getValue;
-  if( mathOperator == "/" ) console.log(getValue);return display.value = initVal / getValue;
+const factorial = function () {
+  initVal = +display.value;
+  // console.log(initVal);
+
+  let total =1;
+  for (let index = 0; index < initVal; index++) {
+
+    total = total * (initVal - 1);
+  }
+  // return (initVal != 1) ? initVal * factorial(initVal - 1) : 1;
+  return total;
 }
+
+
+
+const equal = function () {
+  // if()
+  let getValue = +display.value;
+  if (mathOperator == "+") { capture.textContent = ""; return display.value = initVal + getValue; }
+  if (mathOperator == "-") { capture.textContent = ""; return display.value = initVal - getValue; }
+  if (mathOperator == "*") { capture.textContent = ""; return display.value = initVal * getValue; }
+  if (mathOperator == "/") { capture.textContent = ""; return display.value = initVal / getValue; }
+  if (mathOperator == "Mod") { capture.textContent = ""; return display.value = initVal % getValue; }
+  // if (mathOperator == "!") { capture.textContent = ""; return display.value = factorial(); }
+
+  // if (mathOperator == "(") { capture.textContent = ""; return display .value = initVal * getValue; }
+  // if (mathOperator == ")") { capture.textContent = ""; return display.value = initVal / getValue; }
+} 
